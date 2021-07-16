064c6dd7f536eb29f5af83329acef1ade419d726
znah321 <541313324@qq.com>
2021-07-16 20:09:18 +0800

    第二章的笔记以及练习题

---
+++ chapter2/chapter2-3.py
@@ -0,0 +1,25 @@
+# 2-3
+name = 'Albert Einstein'
+print("Hello " + name + ", would you like to learn some Python today?\n")
+
+# 2-4
+print('lower(): ' + name.lower())
+print('upper(): ' + name.upper())
+print('title(): ' + name.title() + '\n')
+
+# 2-5
+print(name + ' once said, "A person who never made a mistake never tried anything new."')
+
+# 2-6
+print()
+famous_person = name
+message = famous_person + ' once said, "A person who never made a mistake never tried anything new."'
+print(message)
+
+# 2-7
+print()
+name1 = '\t\n      LiHua                         \n'
+print('lstrip(): ' + name1.lstrip())
+print('rstrip(): ' + name1.rstrip())
+print('strip(): ' + name1.strip())
+
---
+++ chapter2/chapter2-4.py
@@ -0,0 +1,10 @@
+# 2-8
+print(1 + 7)
+print(2 * 4)
+print(9 - 1)
+print(16 / 2)
+
+# 2-9
+print()
+number = 7
+print('My favorite number is ' + str(number))
